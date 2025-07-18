59915eb4418a05107b0deb08f8a6bb6f6aca69cf
shama-jain <hshamajain@gmail.com>
2025-07-18 20:03:06 +0530

    deploying in render

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 let expenses = [];
 
@@ -35,6 +38,10 @@ app.delete("/api/expenses/:id", (req, res) => {
   res.status(204).send();
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
